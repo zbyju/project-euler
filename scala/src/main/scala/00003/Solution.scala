6457ac9a9491d3be0fd723e5fe45a3be8fa7d99b
zbyju <zbynek.jurica@gmail.com>
2022-12-27 13:56:29 +0100

    Format

--- scala/src/main/scala/00003/Solution.scala
+++ scala/src/main/scala/00003/Solution.scala
@@ -1,5 +1,7 @@
 package `00003`
 
+import java.math.BigInteger
+
 def isPrime(x: Int): Boolean = {
   val end: Int = math.sqrt(x).toInt
   for (i <- 2 to math.sqrt(x).toInt) {
@@ -10,7 +12,7 @@ def isPrime(x: Int): Boolean = {
   return true
 }
 
-def findFactor(x:Long, from: Int = 2): Int = {
+def findFactor(x: Long, from: Int = 2): Int = {
   if (from == 2 && x % 2 == 0) {
     return 2
   }
@@ -23,17 +25,18 @@ def findFactor(x:Long, from: Int = 2): Int = {
   return -1
 }
 
-def findFactors(x:Long): Seq[Int] = {
+def findFactors(x: Long): Seq[Int] = {
   if (x <= 1) return Seq()
   val factor = findFactor(x)
   return findFactors(x / factor) :+ factor
 }
 
-def run(x:Long): Int = {
+def run(x: Long): Int = {
   findFactors(x).max
 }
 
 @main def solve: Unit = {
   println(run(13195))
-  println(run(600851475143))
+  val x = BigInt(new BigInteger("600851475143"))
+  println(run(x.toLong))
 }
